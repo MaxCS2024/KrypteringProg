c0b7df09502df6ac591df42e521a39b8ee00e823
Max <maxg04@outlook.com>
2024-11-11 15:37:05 +0100

    initial commit

--- KursProjekt.py
+++ KursProjekt.py
@@ -2,8 +2,7 @@ import os
 import argparse
 from cryptography.fernet import Fernet
 
-## Skapar nyckeln som vi kommer att använda för att krypera samn dekryptera filer ##
-
+## Skapar nyckeln som vi kommer att använda för att krypera samt dekryptera filer ##
 def generate_key():
     key = Fernet.generate_key()
     with open("Secret.key", "wb") as key_file:
@@ -11,12 +10,12 @@ def generate_key():
         print("Key has been generated!")
         return key
 
-
+## Hämtar nyckeln så att dem andra funktionerna kan använda den ##
 def load_key():
     with open('Secret.key', "rb") as key_file:
         return key_file.read()
 
-
+##  Funktionen som krypterar filen med nyckeln ##
 def encrypt_file(filename, key):
     cipher_suite = Fernet(key)
 
@@ -41,7 +40,7 @@ def encrypt_file(filename, key):
     print(f"File '{filename}' har blivit krypterad! ")       
 
 
-
+## Funktionen som dekrypterar filen med nyckeln ## 
 def decrypt_file(encrypted_filename, key):
     cipher = Fernet(key)
 
@@ -63,7 +62,6 @@ def decrypt_file(encrypted_filename, key):
         print(f"{file_path} finns inte! ")
         ## Test code ##
 
-
     print(f"Fil {encrypted_file} har blivit dekrypterad")
 
 ## Argparse delen av koden som tar hand om dem olika alternativen :)
@@ -72,7 +70,7 @@ parser.add_argument("action", choices=["encrypt", "decrypt", "generate-key"], he
 parser.add_argument("filename", nargs="?", help="Ange den fil du vill kryptera!")
 args = parser.parse_args()
 
-
+## Kollar igenom vad du vill göra och svarar enligt alternativet ##
 if args.action == "generate-key":
     generate_key()
 elif args.action == "encrypt":
